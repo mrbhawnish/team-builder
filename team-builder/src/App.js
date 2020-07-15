118619b00f28da16ef0e7deb2ecd493754663eb2
bhawnishk1 <mrbhawnish@gmail.com>
2020-07-15 14:53:49 -0700

    MVP completed with inputs rendering

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 import Form from "./components/Form";
 
-
+//Higher component
 function App() {
 
   const defaultState = [
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -17,6 +17,7 @@ function Form(props){
     props.setMember([...props.member, { name, email, role}])  
     } 
    }
+   //Inputs below
    return(
        <div className="form">
        <form onSubmit={handleSubmit}>
